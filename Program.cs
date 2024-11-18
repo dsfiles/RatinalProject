44e8a373f55209f8240612d7d51d3979826399a9
gli <gli@SMU13041>
2024-11-18 09:40:22 -0800

    final update

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace RatinalProject
-{
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Rational project");
-        }
+            Console.WriteLine("--- Rational project ---");
+            Rational r1 = new Rational();
+            Rational r2 = new Rational(2, 6);
+           
+            r2.WriteRational3();
+            r2.Reduce();
+            r2.WriteRational3();
+
+
+        /*
+    public void Reduce(Rational r)
+    {
+        int gcd = GCD(r.numerator, r.denominator);
+        r.numerator /= gcd;
+        r.denominator /= gcd;
+    }
+         */
+
     }
 }
+    
+
---
+++ Rational.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Rational
+    {
+        public int numerator, denominator;
+        public Rational()
+        {
+            numerator = 0;
+            denominator = 1;
+        }
+        public Rational(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        public static void WriteRational(Rational r)
+        {
+            //Console.WriteLine(r);
+            Console.WriteLine($"{r.numerator}/{r.denominator}");
+        }
+        public void WriteRational2(Rational r)
+        {
+            Console.WriteLine($"{r.numerator}/{r.denominator}");
+        }
+
+        public void WriteRational3()
+        {
+            Console.WriteLine($"{this.numerator}/{this.denominator}");
+        }
+
+    public void Reduce()
+    {
+        int gcd = GCD(Math.Abs(numerator), Math.Abs(denominator));
+        this.numerator /= gcd;
+        this.denominator /= gcd;
+    }
+
+        static int GCD(int n1, int n2)
+        {
+            if (n2 == 0)
+            {
+                return n1;
+            }
+            else
+            {
+                return GCD(n2, n1 % n2);
+            }
+        }
+
+}
